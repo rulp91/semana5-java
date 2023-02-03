b47c2fa12806be56ca3d0d64a2484cbf5db804de
rperez_coaat <rperez@coaat.es>
2023-02-03 20:35:47 +0100

    Creación de test

---
+++ src/test/java/es/master/isia/controller/MainViewControllerTest.java
@@ -0,0 +1,20 @@
+package es.master.isia.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainViewControllerTest {
+
+    @Test
+    void getInstance() {
+    }
+
+    @Test
+    void showDirChooser() {
+    }
+
+    @Test
+    void onEvent() {
+    }
+}
